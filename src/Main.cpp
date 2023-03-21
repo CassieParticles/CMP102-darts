9dff2907351543a3271c5b60fb0890ccfbc2249f
Joseph Parsons <Joseph.Parsons2003@outlook.com>
2023-03-21 16:08:23 +0000

    Added painful logic for throwing darts well

--- src/Main.cpp
+++ src/Main.cpp
@@ -47,11 +47,130 @@ int playGame501(char playerFirst)
 	Avoid ending up below 10, if they are below 10 on their final dart, aim high to discount set
 		Aiming for single 14 is best, all neighbors are 9 or more, so guarenteed to revert
 	*/
-	Dartboard501 testBoard{ 50,25 };
-	Player501 testPlayer{ "Tess",.73f,.8f,.4f,.3f,0.3f,501,&testBoard };
-	Player501::Target target{ 50,2 };
-	
-	std::cout<<"New score: "<<testPlayer.throwDart(target)<<'\n';
+	Dartboard501 board{ 50,25 };
+
+	Player501 Joe{ "Joe",0.71f,0.8f,0.9f,0.4f,0.3f,501,&board };
+	Player501 Sid{ "Sid",0.73f,0.8f,0.93f,0.41f,0.31f,501,&board };
+
+	Player501* playerOrder[2];	//Same setting up player order as before
+	int cPlayerIndex = 0;
+
+	if (playerFirst == 'S')
+	{
+		playerOrder[0] = &Sid;
+		playerOrder[1] = &Joe;
+	}
+	else
+	{
+		playerOrder[0] = &Joe;
+		playerOrder[1] = &Sid;
+	}
+
+	bool playerWon = false;
+
+	while(true)	//Will run until break is called, break is called when someone wins
+	{
+		Player501* currentPlayer = playerOrder[cPlayerIndex];
+		for (int i = 0; i < 3; i++)	//Make first 3 throws
+		{
+			if (currentPlayer->getScore() > 180)	//No way to checkout this turn if score is above 180
+			{
+				currentPlayer->throwDart({ 20,3 });	//Aim for the highest score (triple 20)
+				continue;
+			}
+			if (i == 0)	//1st dart, aim to get within single range of bull, if not, just aim hight
+			{
+				int innerBullseyeDiff = currentPlayer->getScore() - board.getInnerBullseye();
+				int outerBullseyeDiff = currentPlayer->getScore() - board.getOuterBullseye();
+
+				if (innerBullseyeDiff < 0) { innerBullseyeDiff = 99999999999999; }	//If player score is less then bullseye, make the bullseye very undesirable
+				if (outerBullseyeDiff < 0) { outerBullseyeDiff = 99999999999999; }
+
+				if (innerBullseyeDiff < 20)
+				{
+					currentPlayer->throwDart({ innerBullseyeDiff,1 });
+				}
+				else if (outerBullseyeDiff < 20)
+				{
+					currentPlayer->throwDart({ outerBullseyeDiff,1 });
+				}
+				else
+				{
+					currentPlayer->throwDart({ 20,3 });	//Aim for the highest score (triple 20)
+				}
+			}
+			if (i == 1)	//2nd dart, try to get score to a point where the player can checkout (bullseye first, then double)
+			{
+				if (currentPlayer->getScore() > 110)	//Cannot get to checkout point
+				{
+					currentPlayer->throwDart({ 20,3 });	//Aim for the highest score (triple 20)
+					continue;
+				}
+				//Check the bullseye
+				int innerBullseyeDiff = currentPlayer->getScore() - board.getInnerBullseye();
+				int outerBullseyeDiff = currentPlayer->getScore() - board.getOuterBullseye();
+
+				if (innerBullseyeDiff < 0) { innerBullseyeDiff = 99999999999999; }	//If player score is less then bullseye, make the bullseye very undesirable
+				if (outerBullseyeDiff < 0) { outerBullseyeDiff = 99999999999999; }
+
+				if (innerBullseyeDiff < 20) { currentPlayer->throwDart({ innerBullseyeDiff,1 }); }	//If player can get to checkout with single, do so
+				else if (outerBullseyeDiff < 20) { currentPlayer->throwDart({ outerBullseyeDiff,1 }); }
+
+				else if (innerBullseyeDiff == board.getInnerBullseye() || outerBullseyeDiff == board.getInnerBullseye())	//Inner bullseye will lead to bullseye checkout
+				{
+					currentPlayer->throwDart({ board.getInnerBullseye(),1 });
+				}
+				else if (innerBullseyeDiff == board.getOuterBullseye() || outerBullseyeDiff == board.getOuterBullseye())	//Outer bullseye will lead to bullseye checkout
+				{
+					currentPlayer->throwDart({ board.getOuterBullseye(),1 });
+				}
+
+				else if (innerBullseyeDiff < 40 && innerBullseyeDiff % 2 == 0) { currentPlayer->throwDart({ innerBullseyeDiff /2,2}); }	//Double to get to bullseye checkout
+				else if (outerBullseyeDiff < 40 && outerBullseyeDiff % 2 == 0) { currentPlayer->throwDart({ outerBullseyeDiff /2,2}); }
+
+				else if (innerBullseyeDiff < 60 && innerBullseyeDiff % 3 == 0) { currentPlayer->throwDart({ innerBullseyeDiff / 3,3 }); }	//Triple to get to bullseye checkout
+				else if (outerBullseyeDiff < 60 && outerBullseyeDiff % 3 == 0) { currentPlayer->throwDart({ outerBullseyeDiff / 3,3 }); }
+
+				//Player cannot checkout with a bullseye, try for double
+				else if (currentPlayer->getScore() < 40)	//Player already in "double" range, try to keep value even
+				{
+					currentPlayer->throwDart({ 6,2 });	//Low score, has even neighbor, and aiming for double means chance odd neighbor will be even anyway
+				}
+				else//Any time the player could get to the "double" range, they would've been able to get to the bullseye range
+				{
+					currentPlayer->throwDart({ 20,3 });	//Aim to get score down faster
+				}
+			}
+			if (i == 2)	//3rd dart, keep score above 10, checkout if so and possible
+			{
+				if (currentPlayer->getScore() < 10)	//prevent score from going below 10
+				{
+					currentPlayer->throwDart({ 14,1 });	//Ensures score is reset
+				}
+				else if (currentPlayer->getScore() == 50 || currentPlayer->getScore() == 25)	//Checkout by bullseye
+				{
+					currentPlayer->throwDart({ currentPlayer->getScore(),1 });
+				}
+				else if (currentPlayer->getScore() < 40 && currentPlayer->getScore()%2==0)	//checkout by double
+				{
+					currentPlayer->throwDart({ currentPlayer->getScore() / 2, 2 });
+				}//Cannot checkout if at this point
+				else if (currentPlayer->getScore() < 20)
+				{
+					currentPlayer->throwDart({ 20,1 });	//Keep player score higher, to reduce chance of getting stuck
+				}
+				else if (currentPlayer->getScore() < 40)
+				{
+					currentPlayer->throwDart({ 1,1 });	//Try to reduce score as little as possible
+				}
+				else
+				{
+					currentPlayer->throwDart({ 14,1 });	//Middle score, reducing score, but not 
+				}
+			}
+		}
+		currentPlayer->endTurn();
+	}
 
 	return 0;
 }
